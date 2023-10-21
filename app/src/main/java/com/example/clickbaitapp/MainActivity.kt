5579f45c3124f15534ae97ce241c3027a3c0b79c
Javier <jav.ruiz1@gmail.com>
2023-10-21 13:56:52 -0500

    Updates

--- app/src/main/java/com/example/clickbaitapp/MainActivity.kt
+++ app/src/main/java/com/example/clickbaitapp/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.clickbaitapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.clickbaitapp.data.DataSource
+import com.example.clickbaitapp.model.Cards
 import com.example.clickbaitapp.ui.theme.ClickbaitAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +34,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
+
                 ) {
-                    Greeting("Android")
+                    ClickbaitApp()
                 }
             }
         }
@@ -30,17 +44,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ClickbaitApp() {
+    LazyColumn {
+        items(DataSource.cards) {
+            ClickbaitappItem(cards= it)
+            modifier = Modifier.padding(8.dp)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ClickbaitAppTheme {
-        Greeting("Android")
+fun ClickbaitappItem(cards: Int) {
+
+}
+
+@Composable
+fun clickbaitItem(
+    cards: Cards,
+    modifier: Modifier = Modifier
+) {
+    Card() {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(64.dp)
+                    .padding(8.dp),
+                painter = painterResource(cards.imageResourceId),
+                contentDescription = null
+            )
+
+            Column() {
+                Text(
+                    text = stringResource(cards.name),
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ClickbaitAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+
+}
+
+@Preview
+@Composable
+fun ClickbaitAppPreview() {
+    ClickbaitAppTheme (darkTheme = true) {
+        ClickbaitApp()
     }
 }
