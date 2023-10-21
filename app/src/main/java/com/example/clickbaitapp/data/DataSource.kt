7f071f0982c41d405160b6c5b26d3fd3daf144b7
Javier <jav.ruiz1@gmail.com>
2023-10-21 13:31:33 -0500

    Updates

--- app/src/main/java/com/example/clickbaitapp/data/DataSource.kt
+++ app/src/main/java/com/example/clickbaitapp/data/DataSource.kt
@@ -1,6 +1,22 @@
 package com.example.clickbaitapp.data
 
 import com.example.clickbaitapp.R
-import com.example.clickbaitapp.model.cards
+import com.example.clickbaitapp.model.Cards
+
 object DataSource {
+    val cards = listOf(
+        Cards(R.drawable.cartoons,R.string.Top_Headline1,2,R.string.Top_headline_description_1 ),
+        Cards(R.drawable.celebrity,R.string.Top_Headline2,2,R.string.Top_headline_description_2 ),
+        Cards(R.drawable.diy,R.string.Top_Headline3,2,R.string.Top_headline_description_3),
+        Cards(R.drawable.fantasy,R.string.Top_Headline4,2,R.string.Top_headline_description_4),
+        Cards(R.drawable.junkfood,R.string.Top_Headline5,2,R.string.Top_headline_description_5),
+        Cards(R.drawable.lifehacks,R.string.Top_Headline6,2,R.string.Top_headline_description_6),
+        Cards(R.drawable.pet,R.string.Top_Headline7,2,R.string.Top_headline_description_7),
+        Cards(R.drawable.pizza,R.string.Top_Headline8,2,R.string.Top_headline_description_8),
+        Cards(R.drawable.soulmate,R.string.STop_Headline9,2,R.string.Top_headline_description_9),
+        Cards(R.drawable.travel,R.string.Top_Headline10,2,R.string.Top_headline_description_10),
+    )
+
+
+
 }
